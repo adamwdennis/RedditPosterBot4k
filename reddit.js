1c6e172c10bb461894f11e26c16491daf3a16fef
evanisnor <ewisnor@gmail.com>
2016-02-04 16:13:17 -0400

    Running reposter job

--- reddit.js
+++ reddit.js
@@ -1,34 +1,46 @@
 var reddit = require('redditor');
+      
+var ALLOWED_EXT = [".jpg",".png",".gif"];
 
 String.prototype.endsWith = function(suffix) {
     return this.indexOf(suffix, this.length - suffix.length) !== -1;
 };
 
-reddit.get('/r/all.json', function(err, response) {
-    if(err) throw err;
+var parsePosts = function (posts, allowedExtensions, each, done) {
+  for (var i = 0; i < posts.length; i++) {
+    var post = posts[i].data;
 
-    var posts = response["data"]["children"];
-    var allowedExtensions = [".jpg",".png",".gif"];
+    var url = JSON.stringify(post.url).replace(/['"]+/g, '');
+    var title = JSON.stringify(post.title).replace(/['"]+/g, '');
+    var subreddit = JSON.stringify(post.subreddit).replace(/['"]+/g, '');
 
-    for (var i = 0; i < posts.length; ++i) {
-      var post = posts[i]["data"];
-
-      var url = JSON.stringify(post["url"]).replace(/['"]+/g, '');
-      var title = JSON.stringify(post["title"]).replace(/['"]+/g, '');
-
-      var allowedExtension = false;
-
-      for (var j = 0; j < allowedExtensions.length; ++j) {
-        var extension = allowedExtensions[j];
-        if (url.endsWith(extension)) {
-          allowedExtension = true;
-        }
-      }
-      if (allowedExtension) {
-        console.log("\n--------------------------------\n");
-        console.log(title);
-        console.log(url);
-        console.log("\n--------------------------------\n");
+    for (var j = 0; j < allowedExtensions.length; ++j) {
+      var extension = allowedExtensions[j];
+      if (url.endsWith(extension)) {
+        each(title, subreddit, url);
       }
     }
-});
+  }
+
+  done();
+};
+
+module.exports.getHot = function (subreddit, callback) {
+  console.log("Fetching hot posts from r/" + subreddit);
+  var results = [];
+  reddit.get('/r/'+subreddit+'/hot.json', function(err, response) {
+      if(err) throw err;
+      var posts = response["data"]["children"];
+      parsePosts(posts, ALLOWED_EXT,
+        function (title, subreddit, url) {
+          // Each post
+          results.push({title: title, subreddit: subreddit, url: url});
+        },
+        function () {
+          // Done
+          console.log('Done fetching from ', subreddit);
+          callback(results);
+        }
+      );
+  });
+};
--- server.js
+++ server.js
@@ -1,24 +1,37 @@
 var imgur = require('./imgur.js');
 var scheduler = require('./scheduler');
+var reddit = require('./reddit');
 
-var repostTask = function () {
-  imgur.repost('https://c2.staticflickr.com/2/1657/24436425589_b82295dbb9_k.jpg', function (error, newurl) {
+var imageRepost = function (post, callback) {
+  imgur.repost(post.url, function (error, newUrl) {
     if (error) {
+      callback(error);
       return;
     }
-
-    console.log('New url:', newurl);
+    post.url = newUrl;
+    callback(null, post);
   });
-}
-
-var okTask = function (taskId) {
-  console.log('ok task', taskId);
 };
 
-var okJob = function (jobId) {
-  scheduler.runAllTasks(jobId, [okTask, okTask, okTask], 5000);
+var repostJob = function (jobId) {
+  reddit.getHot('all', function (posts) {
+    for (var i = 0; i < posts.length; i++) {
+      imageRepost(posts[i], function (error, post) {
+        console.log(post);
+      });
+    }
+  });
+
+  // scheduler.runTask(jobId, repostTask);
 };
+
+// var okTask = function (taskId) {
+//   console.log('ok task', taskId);
+// };
+
+// var okJob = function (jobId) {
+//   scheduler.runAllTasks(jobId, [okTask, okTask, okTask], 5000);
+// };
 
 console.log('Shitposter go!')
-// scheduler.runJob(repostTask);
-scheduler.runJob(okJob, 8000);
+scheduler.runJob(repostJob, 120000);
